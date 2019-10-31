8517bbe92f16139a7d032ad32d60113fedc11d84
Stephen <stephen.sant123@gmail.com>
2019-10-31 15:01:31 +1100

    build 0.2

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,8 @@ public class UIManager : MonoBehaviour
 {
     GameManager gm;
 
+    public bool startScene;
+
     public Text scoreText;
     public Text finalScoreText;
     public Slider boostBar;
@@ -17,39 +19,51 @@ public class UIManager : MonoBehaviour
 
     void Start()
     {
-        gm = GameManager.instance;
+        if (!startScene)
+        {
+            gm = GameManager.instance;
+        }
     }
     private void Update()
     {
-        scoreText.text = "Score: \n" + gm.score;
-        boostBar.value = gm.boostAmount;
+        if (!startScene)
+        {
+            scoreText.text = "Score: \n" + gm.score;
+            boostBar.value = gm.boostAmount;
+        }
     }
 
     bool paused;
     public void TogglePause()
     {
-        if (paused)
+        if (!startScene)
         {
-            gamePanel.SetActive(true);
-            pausePanel.SetActive(false);
-            Time.timeScale = 1;
-            paused = false;
-        }
-        else
-        {
-            gamePanel.SetActive(false);
-            pausePanel.SetActive(true);
-            Time.timeScale = 0;
-            paused = true;
+            if (paused)
+            {
+                gamePanel.SetActive(true);
+                pausePanel.SetActive(false);
+                Time.timeScale = 1;
+                paused = false;
+            }
+            else
+            {
+                gamePanel.SetActive(false);
+                pausePanel.SetActive(true);
+                Time.timeScale = 0;
+                paused = true;
+            }
         }
     }
 
     public void OpenGameOverPanel(bool openPanel)
     {
-        Time.timeScale = openPanel == true ? 0 : 1;
-        gameOverPanel.SetActive(openPanel);
-        gamePanel.SetActive(!openPanel);
-        pausePanel.SetActive(false);
+        if (!startScene)
+        {
+            Time.timeScale = openPanel == true ? 0 : 1;
+            gameOverPanel.SetActive(openPanel);
+            gamePanel.SetActive(!openPanel);
+            pausePanel.SetActive(false);
+        }
     }
 
     public void GoToScene(int scene)
