59394abcc377d5e3ae3a20579da2d4f2c5d86dee
Stephen <stephen.sant123@gmail.com>
2022-06-11 21:06:12 +1000

    Update to Unity 2019

--- Assets/Scripts/Character Scripts/Player.cs
+++ Assets/Scripts/Character Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 2;
+    public float moveSpeed = 5;
     public float boostMultiplier = 2;
     Vector2 keptDirection;
     public float maxBoost;
